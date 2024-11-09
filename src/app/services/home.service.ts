b5c7758a4b506acb98e76190380b1ada9946bba6
furiac3lta <colummarcelo@gmail.com>
2024-11-09 19:46:23 -0300

    add home component

---
+++ src/app/services/home.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../common/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeService {
+  private apiUrl:string ="http://localhost:8085/api/v1/home";
+  
+  constructor(private httpClient:HttpClient) { }
+
+  getProducts():Observable<Product[]>{
+    return this.httpClient.get<Product[]>(this.apiUrl);
+  }
+  
+}
